432331e2448411e6dc3a8f6d69af2cdd89c0eb90
Francisco Calderón <fjcalderon@gmail.com>
2025-07-25 17:44:24 -0300

    Revert "Added an optional Peer in payloads"

--- src/message.rs
+++ src/message.rs
@@ -239,9 +239,9 @@ pub struct PaymentFailedInfo {
 #[serde(rename_all = "snake_case")]
 pub enum Payload {
     /// Order
-    Order(SmallOrder, Option<Peer>),
+    Order(SmallOrder),
     /// Payment request
-    PaymentRequest(Option<SmallOrder>, String, Option<Amount>, Option<Peer>),
+    PaymentRequest(Option<SmallOrder>, String, Option<Amount>),
     /// Use to send a message to another user
     TextMessage(String),
     /// Peer information
@@ -318,12 +318,12 @@ impl MessageKind {
     /// Verify if is valid message
     pub fn verify(&self) -> bool {
         match &self.action {
-            Action::NewOrder => matches!(&self.payload, Some(Payload::Order(_, _))),
+            Action::NewOrder => matches!(&self.payload, Some(Payload::Order(_))),
             Action::PayInvoice | Action::AddInvoice => {
                 if self.id.is_none() {
                     return false;
                 }
-                matches!(&self.payload, Some(Payload::PaymentRequest(_, _, _, _)))
+                matches!(&self.payload, Some(Payload::PaymentRequest(_, _, _)))
             }
             Action::TakeSell
             | Action::TakeBuy
@@ -384,7 +384,7 @@ impl MessageKind {
             return None;
         }
         match &self.payload {
-            Some(Payload::Order(o, _)) => Some(o),
+            Some(Payload::Order(o)) => Some(o),
             _ => None,
         }
     }
@@ -397,8 +397,8 @@ impl MessageKind {
             return None;
         }
         match &self.payload {
-            Some(Payload::PaymentRequest(_, pr, _, _)) => Some(pr.to_owned()),
-            Some(Payload::Order(ord, _)) => ord.buyer_invoice.to_owned(),
+            Some(Payload::PaymentRequest(_, pr, _)) => Some(pr.to_owned()),
+            Some(Payload::Order(ord)) => ord.buyer_invoice.to_owned(),
             _ => None,
         }
     }
@@ -408,7 +408,7 @@ impl MessageKind {
             return None;
         }
         match &self.payload {
-            Some(Payload::PaymentRequest(_, _, amount, _)) => *amount,
+            Some(Payload::PaymentRequest(_, _, amount)) => *amount,
             Some(Payload::Amount(amount)) => Some(*amount),
             _ => None,
         }
